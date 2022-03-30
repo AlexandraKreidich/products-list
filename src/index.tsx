d3b6b17afb2e8a098732b72e7bfaef41065b038b
AlexandraKreidich <kreidichalexandra@gmail.com>
2022-03-30 13:35:38 +0200

    added bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
