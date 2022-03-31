6ebedc56fcbc383448fdb286bc695c0774ac3823
AlexandraKreidich <kreidichalexandra@gmail.com>
2022-03-31 21:18:09 +0200

    implemented filter

--- src/shared/utilities/filter.ts
+++ src/shared/utilities/filter.ts
@@ -2,5 +2,11 @@ import { Filter } from './../../app/types';
 import { ProductItem } from './../models/ProductItem';
 
 export const filterItems = (items: ProductItem[], filter: Filter): ProductItem[] => {
-  return [];
+  const filteredItems = items.filter((item: ProductItem) => {
+    const genderFilter: boolean = filter.gender ? item.gender === filter.gender : true;
+    const searchFilter: boolean = filter.searchValue ? (item.gtin.includes(filter.searchValue) || item.title.includes(filter.searchValue)) : true;
+    const salePriceFilter: boolean = filter.salePrice ? item.salePrice < item.price : true;
+    return genderFilter && searchFilter && salePriceFilter;
+  });
+  return filteredItems;
 }
