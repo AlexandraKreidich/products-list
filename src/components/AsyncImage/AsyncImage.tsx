ccc712f257578755dc09273518b94b5277b7ebe7
AlexandraKreidich <kreidichalexandra@gmail.com>
2022-04-03 13:24:24 +0200

    enhanced async image loading

--- src/components/AsyncImage/AsyncImage.tsx
+++ src/components/AsyncImage/AsyncImage.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { IAsyncImageProps } from './types';
 import styles from './AsyncImage.module.css';
+import imagePlaceholder from '../../assets/placeholder-image.png';
 
 export function AsyncImage(props: IAsyncImageProps) {
-  const [loadedSrc, setLoadedSrc] = React.useState<string | null>(null);
+  const [loadedSrc, setLoadedSrc] = useState<string | null>(null);
+  const [loadingError, setLoadingError] = useState<boolean>(false);
 
   const handleLoad = useCallback(
     () => {
@@ -12,22 +14,38 @@ export function AsyncImage(props: IAsyncImageProps) {
     [props.src],
   )
 
-  React.useEffect(() => {
+  const handleError = useCallback(
+    () => {
+      setLoadingError(true)
+    }, []
+  )
+
+  useEffect(() => {
     setLoadedSrc(null);
     if (props.src) {
       const image = new Image();
       image.addEventListener('load', handleLoad);
+      image.addEventListener('error', handleError);
       image.src = props.src;
       return () => {
         image.removeEventListener('load', handleLoad);
+        image.removeEventListener('error', handleError);
       };
     }
-  }, [handleLoad, props.src]);
+  }, [handleError, handleLoad, props.src]);
 
   if (loadedSrc === props.src) {
     return (
       <img
         src={props.src}
+        alt='item'
+        className={`img-thumbnail ${props.isAdditionalImage ? styles.additionalImage : ''}`}></img>
+    );
+  }
+  if (loadingError) {
+    return (
+      <img
+        src={imagePlaceholder}
         alt='item'
         className={`img-thumbnail ${props.isAdditionalImage ? styles.additionalImage : ''}`}></img>
     );
